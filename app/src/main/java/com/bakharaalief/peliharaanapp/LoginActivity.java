fe793a9eaa7874ec2214da112eaa933c516c6c73
bakharaalief <50726376+bakharaalief@users.noreply.github.com>
2021-10-02 17:00:24 +0700

    add login and registr activity

--- app/src/main/java/com/bakharaalief/peliharaanapp/LoginActivity.java
+++ app/src/main/java/com/bakharaalief/peliharaanapp/LoginActivity.java
@@ -2,13 +2,40 @@ package com.bakharaalief.peliharaanapp;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.annotation.SuppressLint;
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.TextView;
 
-public class LoginActivity extends AppCompatActivity {
+import com.google.android.material.button.MaterialButton;
+
+public class LoginActivity extends AppCompatActivity implements View.OnClickListener {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_login);
+
+        //set variable for view
+        MaterialButton loginButton = findViewById(R.id.login_button);
+        TextView registerButton = findViewById(R.id.register_button);
+
+        //loginButton
+        loginButton.setOnClickListener(this);
+        registerButton.setOnClickListener(this);
+    }
+
+    @SuppressLint("NonConstantResourceId")
+    @Override
+    public void onClick(View view) {
+        switch (view.getId()){
+            case R.id.login_button:
+                break;
+            case R.id.register_button:
+                Intent registerIntent = new Intent(this, RegisterActivity.class);
+                startActivity(registerIntent);
+                break;
+        }
     }
 }
--- app/src/main/java/com/bakharaalief/peliharaanapp/MainActivity.java
+++ app/src/main/java/com/bakharaalief/peliharaanapp/MainActivity.java
@@ -2,7 +2,9 @@ package com.bakharaalief.peliharaanapp;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.os.Handler;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -10,5 +12,16 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        //handler to delay
+        new Handler().postDelayed(new Runnable() {
+            @Override
+            public void run() {
+                Intent intent = new Intent(MainActivity.this, LoginActivity.class);
+                startActivity(intent);
+                finish();
+            }
+        }, 2000);
+
     }
 }
